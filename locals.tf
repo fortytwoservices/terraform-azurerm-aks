e4cfcf38801bf987bae52ea36539e52d1a9b0feb
Roberth Strand <roberth.strand@amesto.no>
2022-11-14 09:14:10 +0100

    feat: initial logic for version handling
    Added the possibility to set a specific version, as well as a local variable that defines the version to either that or to the data source that fetches latest Kubernetes version.
    
    Signed-off-by: Roberth Strand <roberth.strand@amesto.no>

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  # Kubernetes version handling
+  kubernetes_version = var.automatic_bump_kubernetes_version.enabled ? var.kubernetes_version : data.azurerm_kubernetes_service_versions.current.latest_version
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "kubernetes_version" {
+  description = "Kubernetes version to use for the cluster"
+  type        = string
+  default     = null
+}
+
+variable "automatic_bump_kubernetes_version" {
+  description = "Automatically bump the Kubernetes version to the latest available version"
+  type = object({
+    enabled         = bool
+    version_prefix  = string
+    include_preview = bool
+  })
+  default = {
+    enabled         = false
+    version_prefix  = "1.23"
+    include_preview = false
+  }
+}
