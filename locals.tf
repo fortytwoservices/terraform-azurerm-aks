02a80fad5fc939c6702547ee289a273d0169e02b
Roberth Strand <roberth.strand@amesto.no>
2022-11-14 09:59:58 +0100

    feat: added standard module input and local variables
    Signed-off-by: Roberth Strand <roberth.strand@amesto.no>

--- locals.tf
+++ locals.tf
@@ -1,4 +1,12 @@
 locals {
   # Kubernetes version handling
   kubernetes_version = var.automatic_bump_kubernetes_version.enabled ? var.kubernetes_version : data.azurerm_kubernetes_service_versions.current.latest_version
+
+  tags = merge(
+    {
+      "source" = "terraform"
+      "module" = "terraform-azurerm-aks"
+    },
+    var.tags,
+  )
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,21 @@
+variable "name" {
+  description = "Name used for the resources created"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group to create the resources in"
+  type        = string
+}
+
 variable "location" {
   description = "The location where all resources will be created"
   type        = string
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resources"
+  type        = map(string)
 }
 
 variable "kubernetes_version" {
