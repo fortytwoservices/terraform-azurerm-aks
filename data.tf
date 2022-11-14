1fca53c4bdf30665cfa0693b5ee00f5d6008346c
Roberth Strand <roberth.strand@amesto.no>
2022-11-14 09:15:09 +0100

    fix: added attributes for data source
    Signed-off-by: Roberth Strand <roberth.strand@amesto.no>

--- data.tf
+++ data.tf
@@ -1,3 +1,5 @@
 data "azurerm_kubernetes_service_versions" "current" {
-  location = var.location
+  location        = var.location
+  version_prefix  = var.automatic_bump_kubernetes_version.version_prefix
+  include_preview = var.automatic_bump_kubernetes_version.include_preview
 }
