180aee4ceef645748df7c22de565940b0fe813ae
Roberth Strand <roberth.strand@amesto.no>
2022-11-14 16:36:39 +0100

    fix: fix kubernetes version local
    Signed-off-by: Roberth Strand <roberth.strand@amesto.no>

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   # Kubernetes version handling
-  kubernetes_version = var.automatic_bump_kubernetes_version.enabled ? var.kubernetes_version : data.azurerm_kubernetes_service_versions.current.latest_version
+  kubernetes_version = var.automatic_bump_kubernetes_version.enabled ? data.azurerm_kubernetes_service_versions.current.latest_version : var.kubernetes_version
 
   tags = merge(
     {
