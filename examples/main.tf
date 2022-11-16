499c6deab672a5d9a02cc6949011b92bb32d69df
Roberth Strand <roberth.strand@amesto.no>
2022-11-16 09:00:26 +0100

    ci: added changelog to triggers
    Signed-off-by: Roberth Strand <roberth.strand@amesto.no>

--- examples/main.tf
+++ examples/main.tf
@@ -1,2 +1,22 @@
-# Example, should give the user an idea about how to use this module.
-# This code is found in the examples directory.
+module "kubernetes" {
+  source = "../"
+
+  name                = "demo-prod-westeu"
+  resource_group_name = "rg-demo-prod-westeu"
+  location            = "westeurope"
+
+  service_principal = {
+    client_id     = "00000000-0000-0000-0000-000000000000"
+    client_secret = "value"
+  }
+
+  automatic_bump_kubernetes_version = {
+    enabled         = true
+    version_prefix  = "1.23"
+    include_preview = false
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
