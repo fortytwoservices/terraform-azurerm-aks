b3406c81da2dd9b5a8c0bf3f2a43529eb3a0d263
Ketil Gjerde <ketil.gjerde@amesto.no>
2023-05-31 09:32:39 +0200

    docs: updated doc and examples
    Signed-off-by: Ketil Gjerde <ketil.gjerde@amesto.no>

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -1,6 +1,6 @@
 module "kubernetes" {
-  source  = "amestofortytwo/aks"
-  version = "1.2.0"
+  source  = "amestofortytwo/aks/aurerm"
+  version = "2.1.0"
 
   name                = "demo-prod-westeu"
   resource_group_name = azurerm_resource_group.demo.name
