0012e83dd5062619593d90a1d40bb6327ffad8c9
Ketil Gjerde <ketil.gjerde@amesto.no>
2023-06-28 20:21:25 +0200

    feat: Adds new output aks_id
    Signed-off-by: Ketil Gjerde <ketil.gjerde@amesto.no>
    
    #35

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "aks_id" {
+  description = "The Kubernetes Managed Cluster ID."
+  value       = azurerm_kubernetes_cluster.main.id
+}
+
 output "host" {
   description = "The Kubernetes cluster server host."
   value       = azurerm_kubernetes_cluster.main.kube_admin_config[0].host
