c29d05fd0a47e9d1ba1ef360f65ac639ffbf0001
Ketil <477141+mysteq@users.noreply.github.com>
2024-10-22 08:58:29 +0200

    docs: update examples (#296)
    Signed-off-by: Ketil Gjerde <ketil.gjerde@fortytwo.io>

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,11 +1,16 @@
 #trivy:ignore:avd-azu-0041
 module "kubernetes" {
   source  = "fortytwoservices/aks/azurerm"
-  version = "3.5.0"
+  version = "4.2.0"
 
   name                = "demo-prod-westeu"
   resource_group_name = azurerm_resource_group.demo.name
   location            = azurerm_resource_group.demo.location
+
+  network_profile = {
+    network_plugin = "azure"
+    network_policy = "azure"
+  }
 
   tags = {
     environment = "production"
